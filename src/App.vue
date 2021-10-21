02f15cd3c97c60aee8214f15cdea21ab3a56c95e
hira0taka <asl125asl74asl415@gmail.com>
2021-10-22 08:44:28 +0900

    user

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="wrapper">
-    <h2 class="text-info">Twitter-Vue3</h2>
-    <div class="content">
-      <Form v-on:tweet-event="tweetAction" />
-      <div style="margin-top: 20px">
-        <Tweet
-          v-for="Tweet in AllTweet"
-          v-bind:TweetObj="Tweet"
-          v-bind:key="Tweet.tweet_id"
-        />
+  <div>
+    <div class="wrapper">
+      <!-- ユーザー情報を渡すイベント -->
+      <SelectUser v-on:selectUser-event="selectUser" />
+      <div class="tweet-contents">
+        <h2 class="text-success">Twitter-Vue3</h2>
+        <div class="content">
+          <!-- ユーザー情報をpropsで受け取る -->
+          <Form v-on:tweet-event="tweetAction" v-bind:user="tweet_user" />
+          <div style="margin-top:20px">
+            <Tweet
+              v-for="Tweet in AllTweet"
+              v-bind:TweetObj="Tweet"
+              v-bind:key="Tweet.tweet_id"
+            />
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -17,38 +24,45 @@
 <script>
 import Form from "./components/Form.vue";
 import Tweet from "./components/Tweet.vue";
+import SelectUser from "./components/SelectUser.vue";
 
 export default {
   name: "App",
   components: {
     Form,
     Tweet,
+    SelectUser,
   },
   data() {
     return {
-      // tweet_idとtweet_bodyを持つ配列AllTweetを作る
-      AllTweet: Array.of({
-        tweet_id: "ユーザー@test",
+      AllTweet: Array.of(
+        // 初回表示用のダミーデータ
+        {
+        tweet_id: 0,
+        tweet_user: {
+          user_id: "TestId",
+          user_name: "ユーザー",
+        },
         tweet_body: "はじめてのツイート",
-      }),
+      }
+      ),
+
+      // 初回ロード時にForm.vueにpropsとして渡し、selectUser()で上書きするためのデータ
+      tweet_user: {
+        user_id: "TestId",
+        user_name: "ユーザー",
+      },
     };
   },
+
   methods: {
     tweetAction(TweetObj) {
-      // AllTweetにTweetObjを追加
       this.AllTweet.push(TweetObj);
+    },
+    selectUser(user) {
+      console.log(user);
+      this.tweet_user = user;
     },
   },
 };
 </script>
-
-<style>
-.wrapper {
-  width: 60%;
-  margin: 20px auto;
-}
-.content {
-  margin: 20px auto;
-  width: 100%;
-}
-</style>
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="form form-group">
-    <input type="text" v-model="TweetObj.tweet_id" class="form-control" placeholder="user name">
+    <!-- <input type="text" v-model="TweetObj.tweet_id" class="form-control" placeholder="user name"> -->
     <textarea
-    name="form"
-    id=""
+      name="form"
+      id=""
       v-model="TweetObj.tweet_body"
       rows="10"
       cols="5"
       class="form-control"
       placeholder="今なにしてる？"
     ></textarea>
-    <br/>
-    <button class="text-white bg-info" v-on:click="doTweet">
+    <br />
+    <button class="text-white bg-info p-2 m-2 rounded" v-on:click="doTweet">
       ツイートする
     </button>
   </div>
@@ -22,17 +22,25 @@ export default {
   name: "Form",
   data() {
     return {
-      TweetObj:{
-        tweet_id:'',
-        tweet_body:'',
+      TweetObj: {
+        tweet_id: "",
+        tweet_user: {
+          user_id: "TestId",
+          user_name: "ユーザー",
+        },
+        tweet_body: "",
       },
-    }
+    };
+  },
+  props: {
+    user: Object,
   },
   methods: {
-    doTweet(){
+    doTweet() {
+      this.Tweetobj.tweet_user = this.user;
       // 第一引数を無名オブジェクト指定すると、TweetObjをコピーした内容の新規オブジェクトを作成できる
-      const tweet = Object.assign({},this.TweetObj)
-      this.$emit('tweet-event',tweet)
+      const tweet = Object.assign({}, this.TweetObj);
+      this.$emit("tweet-event", tweet);
     },
   },
 };
